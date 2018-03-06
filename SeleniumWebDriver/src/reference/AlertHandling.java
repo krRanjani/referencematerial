2667eb7e98c65025705358b3f2e7bc4d9a35d598
Vinay.Kumars <Vinay.Kumars@MA10LP516486.na.wkglobal.com>
2018-03-06 15:45:12 -0500

    First Commit

---
+++ SeleniumWebDriver/src/reference/AlertHandling.java
@@ -0,0 +1,47 @@
+package reference;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class AlertHandling {
+	
+	WebDriver driver = new ChromeDriver();
+	String URL = "http://demo.guru99.com/test/delete_customer.php";
+	
+  @BeforeTest
+  public void launchBrowser() 
+  {
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+  }
+  
+  @Test
+  public void alertHandling() 
+  {
+	  driver.findElement(By.name("cusid")).sendKeys("1234");
+	  driver.findElement(By.name("submit")).click();
+	  Alert qstn = driver.switchTo().alert();
+	  qstn.dismiss();
+	  driver.findElement(By.name("cusid")).clear();
+	  driver.findElement(By.name("cusid")).sendKeys("6787");
+	  driver.findElement(By.name("submit")).click();
+	  System.out.println("Question is: "+qstn.getText());
+	  qstn.accept();
+	  
+	  Alert conf = driver.switchTo().alert();
+	  System.out.println("Message is: "+conf.getText());
+	  conf.accept();
+  }
+
+  @AfterTest
+  public void closeBrowser() 
+  {
+	  driver.close();
+  }
+
+}
