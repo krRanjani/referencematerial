7287487d308679a7baafa9da543ddc013e793aa5
vinay <vinay@192.168.1.7>
2018-07-03 18:19:22 -0400

    Added a sample program Forloop and simple change in Sampleguru

--- SeleniumWebDriver/src/guru99/Controllers/Sampleguru.java
+++ SeleniumWebDriver/src/guru99/Controllers/Sampleguru.java
@@ -1,13 +1,13 @@
 package guru99.Controllers;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+//import java.util.List;
+//import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.Select;
 
 public class Sampleguru {
 	
